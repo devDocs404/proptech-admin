b9f52c555a3c5a66605c0617668282a278b751c0
Yokeshs TS <tsyokeshs@gmail.com>
2026-02-19 01:23:15 +0530

    feat: Email template added with resend (#4)
    Introduced a email template package integrated with Resend.
    Centralizes email template management and improves maintainability across the monorepo.
    No breaking changes.

--- apps/web/src/app/(main)/dashboard/dashboard.tsx
+++ apps/web/src/app/(main)/dashboard/dashboard.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 import type { authClient } from "@/lib/auth-client";
 import { trpc } from "@/utils/trpc";
@@ -10,10 +10,26 @@ export default function Dashboard({
   session: typeof authClient.$Infer.Session;
 }) {
   const privateData = useQuery(trpc.privateData.queryOptions());
+    const toggleMutation = useMutation(
+    trpc.invite.send.mutationOptions({
+      onSuccess: (data) => {
+        console.log("sent", data);
+      },
+      onError: (err) => {
+        console.error("send email error", err);
+      },
+    })
+  );
 
   return (
     <>
       <p>API: {privateData.data?.message}</p>
+       <button
+        className="cursor-pointer rounded-md bg-blue-500 p-2 text-white transition-all duration-300 hover:bg-blue-600"
+        onClick={() => toggleMutation.mutate()}
+      >
+        send email
+      </button>
     </>
   );
 }
---
+++ packages/api/src/lib/email.ts
@@ -0,0 +1,32 @@
+import { Resend } from "resend";
+import { InvitationEmail } from "@proptech-admin/emails";
+import { env } from "@proptech-admin/env/server";
+
+const resend = new Resend(env.RESEND_API_KEY);
+
+const FROM_EMAIL = "Proptech <onboarding@resend.dev>";
+// const FROM_EMAIL = "Proptech <tsyokeshs@gmail.com>";
+
+export async function sendInviteEmail(
+  to: string,
+  userEmail: string,
+  tempPassword: string
+) {
+console.log("Sending invite email to:", to);
+  try {
+    const res = await resend.emails.send({
+      from: FROM_EMAIL,
+      to,
+      subject: "You're invited to Proptech",
+      react: InvitationEmail({
+        userEmail,
+        tempPassword,
+      }),
+    });
+    console.log("Resend send result:", res);
+    return res;
+  } catch (err) {
+    console.error("Resend send error:", err);
+    throw err;
+  }
+}
